3a5f6ca8e008693061ee2449023dcebda24122b4
Hamza Feroz <hamzaferoz89@gmail.com>
2025-07-29 16:05:05 +0530

    supabase_config.py

--- supabase_config.py
+++ supabase_config.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def get_supabase_client() -> Client:
     url = "https://obzztuakskphspfjouvv.supabase.co"
-    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ienp0dWFrc2twaHNwZmpvdXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2OTIwNjgsImV4cCI6MjA2OTI2ODA2OH0.kFZsRSdyF9aQ26_zOTKRpZCnz3dn5vqw5MqtoJPhwL0"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ienp0dWFrc2twaHNwZmpvdXZ2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzY5MjA2OCwiZXhwIjoyMDY5MjY4MDY4fQ.xSKPwny5b6DXxKNBtWSZ1JJQta3aqdNYfnvTI4xQ0p0"
     supabase = create_client(url, key)
     # Restore session if available
     if "access_token" in st.session_state:
